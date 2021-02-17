088c5dac8bce976e1a7610e7616297295688148d
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2021-02-17 01:01:55 +0100

    Barcode scanning

--- Item Count/app.py
+++ Item Count/app.py
@@ -271,4 +271,4 @@ def send_varer():
     return render_template('send_varer.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=False, port=5000)
