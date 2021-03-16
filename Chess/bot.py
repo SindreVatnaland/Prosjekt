f8e85a6f8bb7c9b924ba780b3ded8f395e48a462
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2021-03-16 21:06:37 +0100

    Implemented miximax algorithm

--- Chess/bot.py
+++ Chess/bot.py
@@ -1,80 +1,93 @@
 import chess
+import math
 from time import sleep
 
-points = 0
-opponent_points = 0
-move_from = None
-move_to = None
-opponent_first_move_from = None
-opponent_first_move_to = None
-opponent_color = None
-first_move_from = None
-first_move_to = None
-bot_color = None
-start_depth = 3
+
+class Node:
+    def __init__(self, depth, board, color, player=1):
+        self.depth = depth
+        self.board = board
+        self.color = color
+        self.player = player
+        self.value = self.player * calculate_move(self.board)
+        self.moves = {}
+        self.children = []
+        self.GetMoves()
+        self.CreateChildren()
+
+    def CreateChildren(self):
+        if self.depth >= 0:
+            for from_ in self.moves:
+                if not self.moves[from_] is None:
+                    for move in self.moves[from_]:
+                        self.children.append(Node(self.depth-1, chess.movePiece(from_, move, self.board), change_color(self.color), -self.player))
+
+    def GetMoves(self):
+        for piece in range(64):
+            if chess.getColor(chess.getPiece(piece, self.board)) == self.color:
+                self.moves[piece] = chess.isValid(piece, self.board)
+
+
+def MinMax(board, depth, color):
+    if depth == 0 or abs(calculate_move(board)) >= abs(10000):
+        return None, calculate_move(board)
+    moves = get_moves(board, color)
+    best_move = None
+
+    if color == chess.Color.black:
+        best_score = -math.inf
+        for from_move in moves:
+            for to_move in moves[from_move]:
+                temp_board = chess.movePiece(from_move, to_move, board)
+                score = MinMax(temp_board, depth-1, change_color(color))[1]
+                best_score = max(best_score, score)
+                if best_score == score:
+                    best_score = score
+                    best_move = (from_move, to_move)
+        return best_move, best_score
+    else:
+        lowest_score = math.inf
+        for from_move in moves:
+            for to_move in moves[from_move]:
+                temp_board = chess.movePiece(from_move, to_move, board)
+                score = MinMax(temp_board, depth-1, change_color(color))[1]
+                lowest_score = min(lowest_score, score)
+                if lowest_score == score:
+                    lowest_score = score
+                    best_move = (from_move, to_move)
+        return best_move, lowest_score
 
 
 def find_move(board, color):
-    global move_from
-    global move_to
-    global start_depth
-    global points
-    points = -99999
-    move_from = None
-    move_to = None
-    generation(board, start_depth, color)
+    # tree = Node(start_depth, board, color)
+    move, score = MinMax(board, 3, color)
 
-    return move_from, move_to
+    # generation(board, start_depth, color)
 
-
-def generation(board, k, color):
-    global points
-    global start_depth
-    global move_from
-    global move_to
-    global opponent_first_move_from
-    global opponent_first_move_to
-    global first_move_from
-    global first_move_to
-    if k > 0:
-        moves = get_moves(board, color)
-        for from_ in moves:
-            if not moves[from_] is None:
-                for move in moves[from_]:
-                    if start_depth == k:
-                        first_move_from = from_
-                        first_move_to = move
-                    elif start_depth - 1:
-                        opponent_first_move_from = from_
-                        opponent_first_move_to = move
-                    generation(chess.movePiece(from_, move, board), k-1, color)
-    else:
-        if calculate_move(board, color) >= points:
-            points = calculate_move(board, color)
-            print(points)
-            move_from = first_move_from
-            move_to = first_move_to
+    return move
 
 
 
-def calculate_move(board, color):
+
+def calculate_move(board):
     score = 0
     pieces = find_pieces(board)
     for piece in pieces:
-        if chess.getColor(piece) == color:
-            multiplier = 1
+        if chess.isWhite(chess.getColor(piece)):
+            multiply = -1
         else:
-            multiplier = -1
+            multiply = 1
+
         if chess.isPawn(piece):
-            score += multiplier * 100
+            score += multiply * 10
         elif chess.isKnight(piece) or chess.isBishop(piece):
-            score += multiplier * 300
+            score += multiply * 30
         elif chess.isRook(piece):
-            score += multiplier * 500
+            score += multiply * 50
         elif chess.isQueen(piece):
-            score += multiplier * 800
+            score += multiply * 90
         elif chess.isKing(piece):
-            score += multiplier * 10000
+            score += multiply * 1000
     return score
 
 
@@ -85,7 +98,7 @@ def calculate_move_last(board):
 def get_moves(board, color):
     moves = {}
     for piece in range(64):
-        if chess.getColor(chess.getPiece(piece, board)) == color:
+        if chess.getColor(chess.getPiece(piece, board)) == color and chess.getPiece(piece, board):
             moves[piece] = chess.isValid(piece, board)
     return moves
 
